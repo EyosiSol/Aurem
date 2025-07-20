f787ea04d7e5e71a06b34f55ac41a688c951d555
EyosiSol <115923891+EyosiSol@users.noreply.github.com>
2025-07-20 11:44:23 +0300

    init: initalize my project workspace using webpack and babel and install typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello React with Webpack! Damn this thing good</h1>
+      <div className="bg-black text-white p-4 rounded-xl shadow-lg">
+        Tailwind 4 !
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"], // Allows importing without file extension
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: "./dist",
+    hot: true,
+    port: 3000,
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  mode: "development",
+};
